fb98333a00bac17ff782c7f2782bbc34374917fa
AritonDenis <aritondenis1999@yahoo.com>
2024-05-21 03:43:52 +0300

    S-a creat logica pentru ce ar trebui sa vada fiecare utilizator in functie de rolul acestuia!

--- DatasetsFactory/administrator/admin.py
+++ DatasetsFactory/administrator/admin.py
@@ -7,8 +7,28 @@ from DatasetsFactory.models import Groups
 admin_blueprint = Blueprint('admin', __name__)
 
 
-@admin_blueprint.route('groups', methods=['GET', 'POST'])
+@admin_blueprint.route('create-groups', methods=['GET', 'POST'])
 @login_required
-def groups():
+def create_groups():
+    form_create_group = CreateGroup()
+    return render_template('admin/groups.html',
+                           cur_object=current_user,
+                           form_create_group=form_create_group)
 
-    return render_template('admin/groups.html')
+
+@admin_blueprint.route('asign-group', methods=['GET', 'POST'])
+@login_required
+def asign_group():
+    form_select_group = SelectGroup()
+    return render_template('admin/users.html',
+                           cur_object=current_user,
+                           form_create_group=form_select_group)
+
+
+@admin_blueprint.route('privileges-on-datasets', methods=['GET', 'POST'])
+@login_required
+def asign_privile():
+    form_select_group = SelectGroup()
+    return render_template('admin/datasets_pivileges.html',
+                           cur_object=current_user,
+                           form_create_group=form_select_group)
